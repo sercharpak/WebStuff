1b5660278583adcccdd583085d177739fb59311b
Sergio Daniel Hernández Charpak <sergiocharpak@gmail.com>
2016-10-02 20:53:08 -0500

    Implementing JSF + AJAX
    Followed https://www.mkyong.com/jsf2/jsf-2-0-ajax-hello-world-example/

--- SergioDanielHernandezCharpak - JavierAlejandroPerezFernandez - Taller - 5/Taller5/src/java/core/CoreBean.java
+++ SergioDanielHernandezCharpak - JavierAlejandroPerezFernandez - Taller - 5/Taller5/src/java/core/CoreBean.java
@@ -7,22 +7,42 @@ package core;
 
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 /**
  *
  * @author javier
  */
-@Named(value = "coreBean")
-@Dependent
+//@Named(value = "coreBean")
+//@Dependent
+@ManagedBean
+@SessionScoped
 public class CoreBean {
 
     // VARIABLES LOCALES
-    
+    private static final long serialVersionUID = 1L;
+	
+    private String name;
     
     // CONSTRUCTOR
-    public CoreBean() {
-    }
+   // public CoreBean() {
+    //}
     
     
     // METODOS
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    public String getLists(){
+        if("".equals(name) || name ==null){
+            return "";
+	}else{
+            return "The lists:" + name;
+	}
+    }
 }
