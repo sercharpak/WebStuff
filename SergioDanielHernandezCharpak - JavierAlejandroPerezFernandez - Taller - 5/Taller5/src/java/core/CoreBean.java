0168a59c5ec33ba642b8d0ca15e846696814b20f
Javier Alejandro Pérez Fernández <japf0727@gmail.com>
2016-10-02 19:53:01 -0500

    Proyecto principal del Taller 5

---
+++ SergioDanielHernandezCharpak - JavierAlejandroPerezFernandez - Taller - 5/Taller5/src/java/core/CoreBean.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author javier
+ */
+@Named(value = "coreBean")
+@Dependent
+public class CoreBean {
+
+    // VARIABLES LOCALES
+    
+    
+    // CONSTRUCTOR
+    public CoreBean() {
+    }
+    
+    
+    // METODOS
+}
